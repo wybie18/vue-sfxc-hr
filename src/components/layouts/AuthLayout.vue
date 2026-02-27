1c22c17832257b70cd9aaef5182bef71b5096c83
jedtangente <tangencygaming2021@gmail.com>
2026-02-27 15:54:43 +0800

    feat: restructure AuthLayout component with SidebarProvider and AppSidebar integration

--- src/components/layouts/AuthLayout.vue
+++ src/components/layouts/AuthLayout.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
-
 <template>
-    <div>
-        <slot />
-    </div>
+  <div class="fixed inset-0 flex flex-col bg-gray-50">
+    <SidebarProvider class="flex-1 overflow-hidden">
+      <Header />
+      <div class="flex h-full w-full">
+        <AppSidebar class="pt-20" />
+
+        <main class="flex-1 overflow-y-auto bg-background pt-15 pl-3 md:pt-15 pl-3">
+          <div class="md:p-8 min-h-full">
+            <slot />
+          </div>
+        </main>
+      </div>
+    </SidebarProvider>
+  </div>
 </template>
+
+<script setup lang="ts">
+import AppSidebar from '@/components/ui/custom/AppSidebar.vue'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import Header from '@/components/ui/custom/Header.vue'
+</script>
